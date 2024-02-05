cd34752359977c6ee234f4a947ba8ec1cb0448ef
kwondaeho <trewq231@naver.com>
2024-02-05 23:18:55 +0900

    study spring mvc

--- src/main/java/com/example/demobootmvc/Person.java
+++ src/main/java/com/example/demobootmvc/Person.java
@@ -1,10 +1,21 @@
 package com.example.demobootmvc;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 public class Person {
-    String name;
+    private Long id;
+    private String name;
 
     public String getName() {
         return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {
--- src/main/java/com/example/demobootmvc/SampleController.java
+++ src/main/java/com/example/demobootmvc/SampleController.java
@@ -1,9 +1,6 @@
 package com.example.demobootmvc;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SampleController {
@@ -15,5 +12,15 @@ public class SampleController {
     @GetMapping("/hello")
     public String helloV2(@RequestParam("name") Person person){
         return "hello " + person.getName();
+    }
+
+    @GetMapping("/message")
+    public String getPerson(@RequestBody String body){
+        return body;
+    }
+
+    @GetMapping("/jsonMessage")
+    public Person jsonMessage(@RequestBody Person person){
+        return person;
     }
 }
